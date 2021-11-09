9b615e8f4c1a3d7765a76d5e88f7cbecd7a33a8c
Duc Vu <minhducnael00@gmail.com>
2021-11-09 00:32:47 -0500

    Add cart (no logic for item add to cart)

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import json
 
 import flask
 from flask import jsonify, render_template, redirect, flash, request
+from flask.helpers import url_for
+
+import stripe
+
+stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 
 app = flask.Flask(__name__, static_folder="./build/static")
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
@@ -17,6 +22,30 @@ def home():
 
 
 app.register_blueprint(bp)
+
+# Do not remove, Stripe handling api.
+# @app.route("/create-checkout-session", methods=["POST"])
+# def create_checkout_session():
+#     try:
+#         checkout_session = stripe.checkout.Session.create(
+#             line_items=[
+#                 {
+#                     # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+#                     "price": "price_1JrXhtJ2O3RVC57ZFhuSMEgu",
+#                     "quantity": 1,
+#                 },
+#             ],
+#             payment_method_types=[
+#                 "card",
+#             ],
+#             mode="payment",
+#             success_url=request.base_url + "/success.html",
+#             cancel_url=url_for('home', _external=True),
+#         )
+#     except Exception as e:
+#         return str(e)
+
+#     return redirect(checkout_session.url, code=303)
 
 
 app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8081)), debug=True)
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Cart from './Cart';
 
 function Dashboard (props) {
     const products = props.item;
+    const [showItem, setShowItem] = useState(false)
     
     return (
         <>
@@ -11,21 +13,30 @@ function Dashboard (props) {
 
                 <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                 {products.map((product, id) => (
-                    <a key={id} href='/#' className="group">
-                    <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                        <img
-                        src={product.image}
-                        alt={product.description}
-                        className="w-full h-full object-center object-cover group-hover:opacity-75"
-                        />
-                    </div>
-                    <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-                    <p className="mt-1 text-lg font-medium text-gray-900">${product.price}</p>
-                    </a>
+                        <>
+                        <a key={id} href='/#' className="group">
+                        <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                            <img
+                            src={product.image}
+                            alt={product.description}
+                            className="w-full h-full object-center object-cover group-hover:opacity-75"
+                            />
+                        </div>
+                        <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+                        <div className="flex">
+                        <p className="flex-auto mt-1 text-lg font-medium text-gray-900">${product.price} </p>
+                        <button class="w-1/4 flex-auto bg-transparent hover:text-indigo-600 items-center justify-center rounded-md border border-gray-300" type="button" onClick={ () => setShowItem(item => !item)}>Add to bag</button>
+                        </div>
+                        </a>
+                        
+                        </>
                 ))}
                 </div>
             </div>
         </div>
+        
+
+        {showItem ? <Cart/>: <> </>}
         </>
     );
 }
