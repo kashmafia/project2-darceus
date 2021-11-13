3a4063a6195b69b95572201b2c15e2c61c6437b7
Kaushik Kolla <kaushikkolla2000@gmail.com>
2021-11-13 14:31:26 -0500

    uncommented stripe code

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv, find_dotenv
 import json
 import flask
 
-# import stripe
+import stripe
 from flask import jsonify, render_template, redirect, flash, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import BYTEA
@@ -31,7 +31,7 @@ if uri.startswith("postgres://"):
 from flask.helpers import url_for
 
 
-# stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
+stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 
 app = flask.Flask(__name__, static_folder="./build/static")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -187,10 +187,7 @@ def login():
                 USER = form.username.data
                 # return dashboard(form.username.data)
                 return redirect(url_for("dashboard"))
-    return flask.render_template(
-        "login.html",
-        form=form,
-    )
+    return flask.render_template("login.html", form=form,)
 
 
 @app.route("/dashboard", methods=["GET", "POST"])
@@ -224,32 +221,27 @@ def register():
 app.register_blueprint(bp)
 
 # Do not remove, Stripe handling api.
-# @app.route("/create-checkout-session", methods=["POST"])
-# def create_checkout_session():
-#     try:
-#         checkout_session = stripe.checkout.Session.create(
-#             line_items=[
-#                 {
-#                     # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
-#                     "price": "price_1JrXhtJ2O3RVC57ZFhuSMEgu",
-#                     "quantity": 1,
-#                 },
-#             ],
-#             payment_method_types=[
-#                 "card",
-#             ],
-#             mode="payment",
-#             success_url=request.base_url + "/success.html",
-#             cancel_url=url_for('home', _external=True),
-#         )
-#     except Exception as e:
-#         return str(e)
+@app.route("/create-checkout-session", methods=["POST"])
+def create_checkout_session():
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+                    "price": "price_1JrXhtJ2O3RVC57ZFhuSMEgu",
+                    "quantity": 1,
+                },
+            ],
+            payment_method_types=["card",],
+            mode="payment",
+            success_url=request.base_url + "/success.html",
+            cancel_url=url_for("home", _external=True),
+        )
+    except Exception as e:
+        return str(e)
 
-#     return redirect(checkout_session.url, code=303)
+    return redirect(checkout_session.url, code=303)
 
 
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8081)), debug=True)
 
-
-# app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8081)), debug=True)
-if __name__ == "__main__":
-    app.run(debug=True)
