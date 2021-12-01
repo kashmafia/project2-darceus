d42b7945040e9c05ccdd8dcc549e34f4fd36b633
Derek yong <derekyong@Dereks-MacBook-Pro.local>
2021-12-01 18:54:23 -0500

    button logic progress

--- src/components/Header.js
+++ src/components/Header.js
@@ -76,7 +76,7 @@ function classNames(...classes) {
 
 
 function Header(props) {
-  return (props.open) ? ( "HELLO WORLD"
+  return (props.open) ? ( ""
     
   ): <Popover className="relative bg-white">
   <div className="max-w-7xl mx-auto px-4 sm:px-6">
@@ -208,7 +208,8 @@ function Header(props) {
       <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
         <a></a>
           <button className="btn ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700" 
-          onClick={() => props.setShowItem(true)}
+          type="button"
+          onClick={() => props.setOpen(true)}
           >Shopping Cart</button>
       </div>
 
