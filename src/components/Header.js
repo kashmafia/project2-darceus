35163ac753c733ec9db77a3516cbb09910582786
Kaushik Kolla <kaushikkolla2000@gmail.com>
2021-12-01 18:21:24 -0500

    user display on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -75,7 +75,7 @@ function classNames(...classes) {
 
 
 
-function Header() {
+function Header({ username }) {
   return (
     <Popover className="relative bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
@@ -204,6 +204,9 @@ function Header() {
               )}
             </Popover>
           </Popover.Group>
+          <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+            <p className="btn ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-300 hover:bg-indigo-400" >Hi, {username}!</p>
+          </div>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
             <form method="POST" action="/logout">
               <button className="btn ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700" >Log Out</button>
