e791b666c506d0c3a545514bd7a86cb31bb34f3a
Duc Vu <minhducnael00@gmail.com>
2021-12-02 00:55:03 -0500

    final adjustment

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -166,12 +166,12 @@ export default function Cart({item, open, setOpen, setCart}) {
                     </div>
                     <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
                     <div className="mt-6">
-                      <button
+                      <a
                         onClick={() =>checkout()}
                         className="flex justify-center items-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                       >
                         Checkout
-                      </button>
+                      </a>
                     </div>
                     <div className="mt-6 flex justify-center text-sm text-center text-gray-500">
                       <p>
