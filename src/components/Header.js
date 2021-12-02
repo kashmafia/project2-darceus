303846c6b0e30f142f6f9a52ec5aabfe97cbf318
Derek yong <derekyong@Dereks-MacBook-Pro.local>
2021-12-01 19:09:59 -0500

    button not working

--- src/components/Header.js
+++ src/components/Header.js
@@ -207,10 +207,12 @@ function Header(props) {
       </Popover.Group>
       <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
         <a></a>
-          <button className="btn ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700" 
+          <button 
           type="button"
+          className="btn ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700" 
           onClick={() => props.setOpen(true)}
-          >Shopping Cart</button>
+          >Shopping Cart
+          </button>
       </div>
 
 
