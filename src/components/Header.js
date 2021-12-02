6cc6fcbff807e26b39f2ef6457b750fcba6b2cb6
Derek yong <derekyong@Dereks-MacBook-Pro.local>
2021-12-01 19:24:43 -0500

    started profile

--- src/components/Header.js
+++ src/components/Header.js
@@ -205,7 +205,7 @@ function Header({ username }) {
             </Popover>
           </Popover.Group>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <p className="btn ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-300 hover:bg-indigo-400" >Hi, {username}!</p>
+            <button className="btn ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-300 hover:bg-indigo-400" >Hi, {username}!</button>
           </div>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
             <form method="POST" action="/logout">
